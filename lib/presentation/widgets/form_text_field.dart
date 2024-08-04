6b304df81178bf764d7b639320c866b64f053053
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2024-08-04 17:52:28 +0530

    refactor: handle contact method input with possible error scenarios

--- lib/presentation/widgets/form_text_field.dart
+++ lib/presentation/widgets/form_text_field.dart
@@ -1,3 +1,4 @@
+import 'package:employee_management_system/data/DTO/contact_method.dart';
 import 'package:flutter/material.dart';
 
 class FormTextField extends StatelessWidget {
@@ -5,17 +6,24 @@ class FormTextField extends StatelessWidget {
     super.key,
     required this.controller,
     required this.labelText,
+    this.textInputType,
     this.icon,
+    this.isContactMethod = false,
+    this.methods
   });
 
   final TextEditingController controller;
   final String labelText;
   final IconData? icon;
+  final TextInputType? textInputType;
+  final bool isContactMethod;
+  final List<ContactMethod>? methods;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
+      keyboardType: textInputType,
       style: const TextStyle(color: Colors.white),
       decoration: InputDecoration(
         labelText: labelText,
@@ -29,9 +37,13 @@ class FormTextField extends StatelessWidget {
         focusedBorder: OutlineInputBorder(borderSide: const BorderSide(color: Colors.white), borderRadius: BorderRadius.circular(10.0))
       ),
       validator: (value) {
-        if (value!.isEmpty) {
+        if (!isContactMethod && value!.isEmpty) {
           return "Please enter your ${labelText.toLowerCase()}";
         } 
+
+        if(isContactMethod && methods!.isEmpty){
+          return "Please enter atleast one contact detail to proceed";
+        }
         return null;
       },
     );
